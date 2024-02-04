f0285aa93a7a9756af758c63581f38b7e73b1aab
jsavargas <jsavargas@gmail.com>
2024-02-04 06:55:01 -0300

    formatter

--- telethon-downloader/download_manager.py
+++ telethon-downloader/download_manager.py
@@ -4,6 +4,7 @@ import os
 import logger
 import configparser
 
+
 class DownloadPathManager:
     def __init__(self, SECTIONS):
         self.SECTIONS = SECTIONS
@@ -11,21 +12,27 @@ class DownloadPathManager:
     def getREGEXPATH(self, filename):
         download_path = None
         try:
-            for REGEX_PATH in self.SECTIONS['REGEX_PATH'] :
-                logger.logger.info(f'getREGEXPATH :::: filename=[{filename}] REGEX_PATH=[{REGEX_PATH}]')
+            for REGEX_PATH in self.SECTIONS["REGEX_PATH"]:
+                logger.logger.info(
+                    f"getREGEXPATH :::: filename=[{filename}] REGEX_PATH=[{REGEX_PATH}]"
+                )
                 flags = 0
                 pattern = REGEX_PATH
-                if re.search(r'/i$', REGEX_PATH):
+                if re.search(r"/i$", REGEX_PATH):
                     pattern = REGEX_PATH[:-2]
                     flags = re.IGNORECASE
-                pattern = pattern.strip('/') 
-                logger.logger.info(f'pattern i :::: filename=[{filename}] pattern=[{pattern}] flags=[{flags}] ')
+                pattern = pattern.strip("/")
+                logger.logger.info(
+                    f"pattern i :::: filename=[{filename}] pattern=[{pattern}] flags=[{flags}] "
+                )
                 if re.search(pattern, filename, flags=flags):
-                    download_path = os.path.join(self.SECTIONS['REGEX_PATH'][REGEX_PATH])
+                    download_path = os.path.join(
+                        self.SECTIONS["REGEX_PATH"][REGEX_PATH]
+                    )
                     return download_path
                     break
             logger.logger.info(f"getREGEXPATH download_path: {download_path}")
         except Exception as e:
             logger.logger.error(f"getREGEXPATH Exception: {e}")
-        
+
         return download_path
--- telethon-downloader/logger.py
+++ telethon-downloader/logger.py
@@ -1,11 +1,9 @@
 import logging
 
 
-
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '[%(asctime)s] %(levelname)-7s %(message)s')
+formatter = logging.Formatter("[%(asctime)s] %(levelname)-7s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
