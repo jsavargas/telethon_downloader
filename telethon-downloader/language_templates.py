7107f48879923c4d1c2243c0c0a6b47854e53ab6
jsavargas <jsavargas@gmail.com>
2024-02-08 12:35:42 -0300

    Enhancement: Copy files if not exist or are different in /config/locale

--- telethon-downloader/language_templates.py
+++ telethon-downloader/language_templates.py
@@ -1,6 +1,8 @@
 # language_templates.py
 
 import os
+import shutil
+import filecmp
 
 
 class LanguageTemplates:
@@ -9,78 +11,41 @@ class LanguageTemplates:
         self.templates = self.load_templates()
 
     def initialize_templates(self):
-        en_templates = [
-            "WELCOME=Telethon Downloader Started: Version: {msg1}",
-            "PROGRESS_CALLBACK_PATH=Download in {path}",
-            "PROGRESS_CALLBACK_PROGRESS=progress: {current:.2f} MB / {total:.2f} MB",
-            "",
-            "MESSAGE_DOWNLOAD=Downloading in: {path}",
-            "",
-            "MESSAGE_DOWNLOAD_FILE=File downloaded in: {downloaded_file}",
-            "MESSAGE_DOWNLOAD_FILE_SIZE=File size: {file_size:.2f} MB",
-            "MESSAGE_DOWNLOAD_COMPLETED=Download completed in: {elapsed_time}",
-            "MESSAGE_DOWNLOAD_SPEED=Average download speed: {speed:.2f} MB/s",
-            "MESSAGE_DOWNLOAD_AT=at: {end_time}",
-            "MESSAGE_DOWNLOAD_FROM_ID=from: {from_id}",
-            "",
-            "MESSAGE_TIMEOUT_EXCEEDED=Download timeout exceeded...",
-            "MESSAGE_EXCEPTION=Download exception...",
-            "MESSAGE_NO_LINKS_DOWNLOAD=No links to download.",
-            "",
-            "HOUR=hour",
-            "MINUTE=minute",
-            "SECOND=second",
-            "",
-            "HOURS=hours",
-            "MINUTES=minutes",
-            "SECONDS=seconds",
-        ]
-
-        es_templates = [
-            "WELCOME=Telethon Downloader Iniciado: Version: {msg1}",
-            "PROGRESS_CALLBACK_PATH=Descarga en {path}",
-            "PROGRESS_CALLBACK_PROGRESS=progreso: {current:.2f} MB / {total:.2f} MB",
-            "",
-            "MESSAGE_DOWNLOAD=Descargando en: {path}",
-            "",
-            "MESSAGE_DOWNLOAD_FILE=Archivo descargado en: {downloaded_file}",
-            "MESSAGE_DOWNLOAD_FILE_SIZE=Tamaño del archivo: {file_size:.2f} MB",
-            "MESSAGE_DOWNLOAD_COMPLETED=Descarga completada en: {elapsed_time}",
-            "MESSAGE_DOWNLOAD_SPEED=Velocidad de descarga promedio: {speed:.2f} MB/s",
-            "MESSAGE_DOWNLOAD_AT=a las: {end_time}",
-            "MESSAGE_DOWNLOAD_FROM_ID=desde: {from_id}",
-            "",
-            "MESSAGE_TIMEOUT_EXCEEDED=Excedido el tiempo de espera de la descarga...",
-            "MESSAGE_EXCEPTION=Excepción durante la descarga...",
-            "MESSAGE_NO_LINKS_DOWNLOAD=No hay enlaces para descargar.",
-            "",
-            "HOUR=hora",
-            "MINUTE=minuto",
-            "SECOND=segundo",
-            "",
-            "HOURS=horas",
-            "MINUTES=minutos",
-            "SECONDS=segundos",
-        ]
-
         locale_dir = os.path.join("/config", "locale")
-        en_template_path = os.path.join(locale_dir, "en_EN.txt")
-        es_template_path = os.path.join(locale_dir, "es_ES.txt")
 
         if not os.path.exists(locale_dir):
             os.makedirs(locale_dir)
 
-        with open(en_template_path, "w", encoding="utf-8") as en_template_file:
-            en_template_file.write("\n".join(en_templates) + "\n")
+        # Rutas de los archivos de plantillas
+        en_template_path_original = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "locale", "en_EN.txt"
+        )
+        es_template_path_original = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "locale", "es_ES.txt"
+        )
 
-        with open(es_template_path, "w", encoding="utf-8") as es_template_file:
-            es_template_file.write("\n".join(es_templates) + "\n")
+        en_template_path_target = os.path.join(locale_dir, "en_EN.txt")
+        es_template_path_target = os.path.join(locale_dir, "es_ES.txt")
+
+        en_template_exists = os.path.isfile(
+            en_template_path_target
+        ) and not filecmp.cmp(en_template_path_original, en_template_path_target)
+        es_template_exists = os.path.isfile(
+            es_template_path_target
+        ) and not filecmp.cmp(es_template_path_original, es_template_path_target)
+
+        if en_template_exists or not os.path.exists(en_template_path_target):
+            shutil.copy2(en_template_path_original, en_template_path_target)
+
+        if es_template_exists or not os.path.exists(es_template_path_target):
+            shutil.copy2(es_template_path_original, es_template_path_target)
 
         template_file_path = os.path.join(locale_dir, f"{self.language}.txt")
 
-        if not os.path.exists(template_file_path):
-            with open(template_file_path, "w", encoding="utf-8") as template_file_path:
-                template_file_path.write("\n".join(en_templates) + "\n")
+        if os.path.exists(en_template_path_target) and not os.path.exists(
+            template_file_path
+        ):
+            shutil.copy2(en_template_path_original, template_file_path)
 
     def load_templates(self):
         locale_dir = os.path.join("/config", "locale")
