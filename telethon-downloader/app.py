286a6f3286a0afddd202faa5bf179df1d1b43b7d
jsavargas <jsavargas@gmail.com>
2025-06-24 14:20:10 -0400

    5.0.0-r1

--- telethon-downloader/app.py
+++ telethon-downloader/app.py
@@ -6,7 +6,6 @@ import re
 import time
 from datetime import datetime
 import logging
-#import uvloop
 import asyncio
 import yt_dlp
 import shutil
@@ -27,13 +26,12 @@ from url_downloader import URLDownloader
 from pending_handler import PendingMessagesHandler
 from info_handler import InfoMessages
 
-#uvloop.install()
 
 logger.info(f"Starting Telegram Downloader Bot Started : {datetime.now():%Y/%m/%d %H:%M:%S}")
 
 class Config:
     def __init__(self):
-        self.BOT_VERSION = "4.1.0-r1"
+        self.BOT_VERSION = "5.0.0-r1"
         self.PYROGRAM_VERSION = pyrogram_version
         self.YT_DLP_VERSION = yt_dlp.version.__version__
 
